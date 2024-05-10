5bebb78ca82254812182a0dc974c35a034e5e9b1
Abdallah mohamed <87903708+Abdallah0115@users.noreply.github.com>
2024-05-10 10:39:47 +0300

    Update views.py

--- dashboard/dash/views.py
+++ dashboard/dash/views.py
@@ -24,9 +24,9 @@ def Login(req):
                 password = req.POST.get('password')
                 user = authenticate(req, username = username, password=password)
                 cust = Cust.objects.filter(email = username)
-                gues = guest.objects.filte(email = username)
+                #gues = guest.objects.filte(email = username)
 
-                if user is not None and not cust and not gues:
+                if user is not None and not cust :
                     login(req, user)
 
                     return redirect('/dash/hello')
